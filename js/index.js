411f6cc249b197001e6aeaef361bb7c6cc128abb
KSrahul <rahul@ampersanddesign.io>
2018-12-06 22:12:04 +0530

    added CIF Files
    added CIF Files

--- js/index.js
+++ js/index.js
@@ -1,80 +1,90 @@
-// For onscroll hamburger menu up
-document.addEventListener("scroll", function() {
-        var scroll = window.scrollY;
-        if ((document.querySelector(".hamContainer").className = "hamContainer") && (scroll > 122)) {
-            document.querySelector(".hamContainer").className = "hamContainer upHam";
-        }
-    })
-    // For onscroll hamburger menu up
+window.onload = function() {
+    // Try to getting all links from nav
+    var sections = document.getElementsByClassName("navLinks");
+    for (var i = 0; i < sections.length; i++) {
+        sections[i].addEventListener("click", navLinks)
+    }
+    // Try to getting all links from nav (End)
 
-
-// For onlick hamburger Animation
-var toggleMenu = document.querySelector(".mobileMenusContainer");
-document.querySelector(".hamContainer").addEventListener("click", function() {
-        toggleMenu.classList.toggle("open");
-        if (toggleMenu.className == "mobileMenusContainer open") {
-            document.body.style.overflow = "hidden";
-            document.querySelector(".overlay").className = "overlay showOverlay";
-            document.querySelector(".hamContainer").className = "hamContainer animate";
-
+    // trying to getting the current id and minus header height so that scroll reach exact position
+    function navLinks() {
+        var ids = (this).getAttribute("href");
+        if ($(window).width() > 768) {
+            $("html,body").animate({
+                scrollTop: $(ids).offset().top - 80
+            }, 500);
         } else {
-            document.body.style.overflow = "scroll";
-            document.querySelector(".overlay").className = "overlay";
-            document.querySelector(".hamContainer").className = "hamContainer";
-
+            $("html,body").animate({
+                scrollTop: $(ids).offset().top
+            }, 500);
         }
+    }
+    // trying to getting the current id and minus header height so that scroll reach exact position (End)
 
-    })
-    // For onlick hamburger Animation
+
+    // Try to geeting, that if screen size is less than 768 that only this code will be work
+
+    // On scroll navigation highlited or active if reach to perticular section
+    if ($(window).width() > 768) {
+        $(window).scroll(function() {
+            var windscroll = $(window).scrollTop() + 80
+            $(".contentStart .pageSection").each(function(i) {
+                if ($(this).position().top <= windscroll) {
+                    $(".pageLinks li a.active").removeClass("active");
+                    $(".pageLinks li a").eq(i).addClass("active");
+                } else {
+                    $(".pageLinks li a").eq(i).removeClass("active");
+                }
+            });
+            // On scroll navigation highlited or active if reach to perticular section (End)
 
 
-// Outside click hamburger menu close
-document.querySelector(".overlay").addEventListener("click", function() {
-        if (toggleMenu.className = "mobileMenusContainer open") {
-            toggleMenu.className = "mobileMenusContainer";
-            document.body.style.overflow = "scroll";
-            document.querySelector(".overlay").className = "overlay";
-            document.querySelector(".hamContainer").className = "hamContainer";
+            var scrollPos = pageYOffset;
+            if (scrollPos >= 1) {
+                document.querySelector(".header").classList.remove("fixed");
+                document.querySelector(".logo").classList.remove("logoSmall");
+            } else {
+                $(".pageLinks li a.active").removeClass("active");
+            }
+
+            // If scroll reach to header on the banner then header should be fixed
+            if (window.pageYOffset > document.querySelector(".header").offsetTop) {
+                document.querySelector(".header").classList.add("fixed");
+                document.querySelector(".logo").classList.add("logoSmall");
+            } else {
+                document.querySelector(".header").classList.remove("fixed");
+                document.querySelector(".logo").classList.remove("logoSmall");
+            }
+            // If scroll reach to header on the banner then header should be fixed (End)
+        }).scroll();
+    }
+    // Try to geeting, that if screen size is less than 768 that only this code will be work (End)
+
+
+    if ($(window).width() < 768) {
+        function topScroll() {
+            if (window.pageYOffset >= 546) {
+                document.querySelector(".backTop").classList.add("showBackTop")
+            } else {
+                document.querySelector(".backTop").classList.remove("showBackTop")
+            }
         }
-    })
-    // Outside click hamburger menu close
+        document.addEventListener("scroll", topScroll);
 
 
-// For Loader
-document.onreadystatechange = function() {
-    setTimeout(function() {
-        if (document.readyState == "complete") {
-            // document.querySelector(".content").className = "content showContent";
-            document.querySelector(".loaderContainer").className = "loaderContainer removeLoader";
-        } else {
-            // document.querySelector(".content").className = "content";
-            document.querySelector(".loaderContainer").className = "loaderContainer";
-        }
-    }, 2500)
+        document.querySelector(".backTop").addEventListener("click", function() {
+            $('html, body').animate({
+                scrollTop: 0
+            }, 500);
+        })
+    }
+    // window.addEventListener("resize", function() {
+    //     if (window.matchMedia("(max-width: 768px)").matches) {
+    //         if ($(window).width() < 768) {
+    //             location.reload();
+    //         } else {
+    //             location.reload();
+    //         }
+    //     }
+    // });
 }
-
-// For Loader
-
-// Find if browser other than chrome
-var isChromium = window.chrome;
-var winNav = window.navigator;
-var vendorName = winNav.vendor;
-var isOpera = typeof window.opr !== "undefined";
-var isIEedge = winNav.userAgent.indexOf("Edge") > -1;
-var isIOSChrome = winNav.userAgent.match("CriOS");
-
-if (isIOSChrome) {
-    console.log('is Google Chrome on IOS');
-    alert('is Google Chrome on IOS');
-} else if (
-    isChromium !== null &&
-    typeof isChromium !== "undefined" &&
-    vendorName === "Google Inc." &&
-    isOpera === false &&
-    isIEedge === false
-) {
-
-} else {
-    alert("May be for now, you will be able see few issues on this browser, but please open it on chrome browser to see the actual result");
-}
-// Find if browser other than chrome
