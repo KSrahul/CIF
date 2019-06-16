0569a4b0f54e82032d47536ec369f4fd9c914ffb
KSrahul <rahul@ampersanddesign.io>
2019-06-16 22:53:43 +0530

    updated content

--- js/index.js
+++ js/index.js
@@ -21,11 +21,9 @@ window.onload = function() {
     }
     // trying to getting the current id and minus header height so that scroll reach exact position (End)
 
-
-    // Try to geeting, that if screen size is less than 768 that only this code will be work
-
-    // On scroll navigation highlited or active if reach to perticular section
+    // Try to geeting, that if screen size is less than 768 than only this code will be work
     if ($(window).width() > 768) {
+        // On scroll navigation highlited or active if reach to perticular section
         $(window).scroll(function() {
             var windscroll = $(window).scrollTop() + 80
             $(".contentStart .pageSection").each(function(i) {
@@ -36,8 +34,7 @@ window.onload = function() {
                     $(".pageLinks li a").eq(i).removeClass("active");
                 }
             });
-            // On scroll navigation highlited or active if reach to perticular section (End)
-
+        // On scroll navigation highlited or active if reach to perticular section (End)
             var scrollPos = pageYOffset;
             if (scrollPos >= 1) {
                 document.querySelector(".header").classList.remove("fixed");
@@ -52,15 +49,16 @@ window.onload = function() {
                 document.querySelector(".logo").classList.add("logoSmall");
             }
             // If scroll reach to header on the banner then header should be fixed (End)
-        }).scroll();
+    }).scroll();
     } else {
         document.querySelector(".header").classList.remove("fixed");
         document.querySelector(".logo").classList.remove("logoSmall");
     }
     // Try to geeting, that if screen size is less than 768 that only this code will be work (End)
 
-
+    // the back to top button visible when screen reached 768px
     if ($(window).width() < 768) {
+        // show the back to top button when scroll height is 546px
         function topScroll() {
             if (window.pageYOffset >= 546) {
                 document.querySelector(".backTop").classList.add("showBackTop")
@@ -69,14 +67,19 @@ window.onload = function() {
             }
         }
         document.addEventListener("scroll", topScroll);
+        // show the back to top button when scroll height is 546px (end)
 
-
+        // when click on back to top button it will take to the top of the page
         document.querySelector(".backTop").addEventListener("click", function() {
             $('html, body').animate({
                 scrollTop: 0
             }, 500);
         })
+        // when click on back to top button it will take to the top of the (end)
     }
+    // the back to top button visible when screen reached 768px
+
+    // reloading.. the website when reached 768px screen for testing.. purpose
     window.addEventListener("resize", function() {
         if (window.matchMedia("(max-width: 768px)").matches) {
             setTimeout(function() {
@@ -88,4 +91,5 @@ window.onload = function() {
             }, 100)
         }
     });
+    // reloading.. the website when reached 768px screen for testing.. purpose (end)
 }
